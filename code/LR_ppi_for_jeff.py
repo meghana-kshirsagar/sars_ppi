fbc28a25333496f1b5257113ceea9ae9de623a8d
meghana <mekshirs@microsoft.com>
2020-05-21 16:23:54 -0700

    updates

--- code/LR_ppi_for_jeff.py
+++ code/LR_ppi_for_jeff.py
@@ -1,61 +1,15 @@
 
-import pandas as pd
 import sys
+import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 #import seaborn as sns
-from sklearn.preprocessing import MinMaxScaler, StandardScaler
-#from interpret import show
-#from interpret.data import ClassHistogram
+
+from sklearn.model_selection import StratifiedKFold
 from sklearn import metrics
-from sklearn.model_selection import train_test_split, cross_validate, StratifiedKFold
-from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-import pickle
-#from interpret.glassbox import ExplainableBoostingClassifier
-#from interpret.perf import ROC
+from utils import do_logreg_paramtuning, normalize_train_test,impute_train_test,imputeX,get_aucpr,get_auc,binarize,get_fmax,get_aucpr_R,get_auc_R,compute_eval_measures,compute_early_prec,get_early_prec,compute_fmax
 
-seed=0
-
-def do_logreg_paramtuning(X_train, y_train, class_wt):
-    reslist = []
-    metric_idx=1  # index where AUC is stored
-    for cval in [1e-5, 1e-4, 1e-3, 1e-2, 0.1, 1, 10, 100, 1000, 10**5]:
-        logreg = LogisticRegression(random_state=0, penalty='l2', C=cval, max_iter=10000, solver='lbfgs')   # class_weight={0:(1-class_wt+0.1), 1:1}
-        cv_results = cross_validate(logreg, X_train, y_train, cv=5, scoring='average_precision')
-        reslist.append((cval, np.mean(cv_results['test_score'])))
-    print(*reslist, sep='\n')
-    reslist = np.asarray(reslist)
-    bestid = np.where(reslist[:,metric_idx]==max(reslist[:,metric_idx]))[0][0]
-    clf = LogisticRegression(random_state=0, penalty='l2', C=reslist[bestid,metric_idx], max_iter=10000, solver='lbfgs')    
-    clf.fit(X_train, y_train)
-    return clf
-
-def normalize_train_test(X_train, X_test):
-    scaler = StandardScaler()
-    scaler.fit(X_train)
-    X_train = scaler.transform(X_train)
-    X_test = scaler.transform(X_test)
-    return X_train, X_test
-
-def get_aucpr(y_true, y_pred, pos_label=1):
-    #fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_true, pred, pos_label=2)
-    precision, recall, thresholds = metrics.precision_recall_curve(y_true, y_pred, pos_label)
-    auc_val = metrics.auc(recall, precision)
-    return auc_val
-
-def get_auc(labels, preds, pos_label=1):
-    fpr, tpr, _ = metrics.roc_curve(labels, preds, pos_label)
-    return metrics.auc(fpr, tpr)
-
-def binarize(y_pred):
-    return [int(x >= 0.5) for x in y_pred]
-
-
-def save_model(ebm, model_file):
-    model_pkl = open(model_file, 'wb')
-    pickle.dump(ebm,model_pkl)
-    model_pkl.close()
 
 if __name__ == "__main__":
    
@@ -125,8 +79,8 @@ if __name__ == "__main__":
     curr_perf += [metrics.accuracy_score(y_test, y_pred)]
     print(metrics.confusion_matrix(y_test, y_pred))
     y_pred = clf.predict_proba(X_test)
-    curr_perf += [get_aucpr(y_test, y_pred[:,1])]
-    curr_perf += [get_auc(y_test, y_pred[:,1])]
+    curr_perf += [get_aucpr_R(y_test, y_pred[:,1])]
+    curr_perf += [get_auc_R(y_test, y_pred[:,1])]
     print("Performance: ",curr_perf)
 
     # predict on larger set, output predictions
